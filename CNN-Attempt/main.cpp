35c059484ca5eaec8f42a9cd25c2d4fb5ac4f258
jhartman <jhartman@sfu.ca>
2016-04-08 19:50:10 -0400

    INIT: Initial Commit

---
+++ CNN-Attempt/main.cpp
@@ -0,0 +1,12 @@
+//////////////////////////////////////////////////////////////////////
+// Main - A CNN Implementation
+
+#include "opencv-3-1/build/include/cv.h"
+
+
+int main()
+{
+
+
+    return 0;
+}
